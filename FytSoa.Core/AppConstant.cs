a06840849b57fa45b5cd59d7dc3531e58b204925
Roc <chengpeng19925@163.com>
2020-08-12 16:28:58 +0800

    修改连接字符串

--- FytSoa.Core/AppConstant.cs
+++ FytSoa.Core/AppConstant.cs
@@ -10,8 +10,8 @@ namespace FytSoa.Core
 
         public const string KUGOU_API_URL = "https://wwwapi.kugou.com/yy/index.php";
 
-        //public const string NET_API_URL = "http://192.168.1.100:8000";
-        public const string NET_API_URL = "http://47.95.194.168:8800";
+        public const string NET_API_URL = "http://192.168.1.100:8000";
+        //public const string NET_API_URL = "http://47.95.194.168:8800";
 
         public const string NET_API_MUSIC_URL = "https://music.163.com/song/media/outer/url?id={0}.mp3";
 
